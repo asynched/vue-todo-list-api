22cc6c7063eaf525b6e903284635aaca8660c595
Eder Lima <devilsouls666@gmail.com>
2022-01-14 13:51:44 -0300

    fix: remove build files from version control

--- build/config/aliases.js
+++
@@ -1,6 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _path = require('path'); var _path2 = _interopRequireDefault(_path);
-var _modulealias = require('module-alias'); var _modulealias2 = _interopRequireDefault(_modulealias);
-
-_modulealias2.default.addAliases({
-  '@': _path2.default.resolve(__dirname, '..'),
-})
--- build/config/environment.js
+++
@@ -1,3 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
-
-_dotenv2.default.config()
