359967f7a09f425f6aaef7736550006b865c92b0
Eder Lima <devilsouls666@gmail.com>
2022-01-22 00:01:32 -0300

    fix: typo :(

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 /**
- * Connects to the Mongo databse
+ * Connects to the Mongo database
  */
 const connectToMongo = async () => {
   await mongoose.connect(process.env.DATABASE_URL)
